faf1530fb5cc153fcff6a8b23a023b20cb75949c
pushpamohan-abhi <pushpamohan.cs@hkbk.edu.in>
2026-03-14 20:14:17 +0530

    Create fetchCSE.js

---
+++ netlify/functions/fetchCSE.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+exports.handler = async () => {
+
+let students=[];
+
+for(let i=1;i<=120;i++){
+
+let num = i.toString().padStart(3,"0");
+let usn = "1BI21CS"+num;
+
+try{
+
+let url=`https://results.vtu.ac.in/result?usn=${usn}`;
+
+let response = await axios.get(url);
+
+let $ = cheerio.load(response.data);
+
+let name=$("#studentName").text();
+let sgpa=$("#sgpa").text();
+
+students.push({
+usn,
+name,
+sgpa
+});
+
+}catch(err){
+console.log("skip",usn);
+}
+
+}
+
+return{
+statusCode:200,
+body:JSON.stringify(students)
+};
+
+};
